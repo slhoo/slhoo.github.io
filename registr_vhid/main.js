9603b811a2c5d5fc1b5e9857caf07cb430cb22ee
slhoo <124089223+slhoo@users.noreply.github.com>
2023-06-17 16:00:05 +0300

    Add files via upload

--- registr_vhid/main.js
+++ registr_vhid/main.js
@@ -53,3 +53,49 @@ function check_user(){
   }
 }
 check_user()
+let status = 'поля реєстрації'
+function changeForm(){
+  if(status == 'поля реєстрації'){
+  form.innerHTML = `
+  <label for="">Пошта</label><br>
+  <input type="text" id="email_vhid"><br><br>
+  <label for="">Пароль</label><br>
+  <input type="text" id="password_vhid"><br><br>
+  <button onclick="ema_pass_vhid()">Увійти</button><br><br>
+  <button onclick="google_vhid()">Вхід за допомогою гугл</button><br><br>
+  <button onclick="changeForm()">У мене ще немає акаунта</button>
+  `
+  status = 'поля входу'
+}else{
+  form.innerHTML = `
+  <label for="">Ім'я</label><br>
+    <input type="text" id="na"><br><br>
+    <label for="">Прізвище</label><br>
+    <input type="text" id="las"><br><br>
+    <label for="">Вік</label><br>
+    <input type="text" id="ag"><br><br>
+    <label for="">Пошта</label><br>
+    <input type="text" id="email"><br><br>
+    <label for="">Пароль</label><br>
+    <input type="text" id="password"><br><br>
+    <button onclick="ema_pass()">Зареєструватись</button><br><br>
+    <button onclick="google_vhid()">Вхід за допомогою гугл</button><br><br>
+    <button onclick="changeForm()">У мене вже є акаунт</button>
+  `
+  status = 'поля реєстрації'
+}
+}
+
+function ema_pass_vhid(){
+  firebase.auth().signInWithEmailAndPassword(email_vhid.value, password_vhid.value)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    localStorage.setItem('login',user.uid)
+    setTimeout(function(){window.location.href = `spa.html?id=${user.uid}`}, 3000)
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
--- registr_vhid/spa.js
+++ registr_vhid/spa.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', async function(){
     let not_found = await axios.get('templates/404.html');
     let home = await axios.get('templates/home.html');
     let books = await axios.get('templates/books.html');
+    let profile = await axios.get('templates/profile.html');
 
     const data = {
         test: 'Pr',
@@ -14,6 +15,20 @@ document.addEventListener('DOMContentLoaded', async function(){
     }
     const NotFound = {
         template: not_found.data 
+    }
+    const Profile = {
+        template: profile.data,
+        methods:{
+            getUserInfo(){
+                db.collection('reg_test').doc(user_id).get().then(res=>{
+                        console.log(res.data())
+                    }
+                )
+            }
+        },
+        mounted(){
+            this.getUserInfo()
+        }
     }
     const Books = {
         template: books.data,
@@ -37,14 +52,23 @@ document.addEventListener('DOMContentLoaded', async function(){
     const routes = {
         '/': Home,
         '/not-found': NotFound,
-        '/books': Books
+        '/books': Books,
+        '/profile': Profile
     }
     const app = {
         data(){
             return data
         },
         methods: {
-            
+            sign_out(){
+                firebase.auth().signOut().then(() => {
+                  }).catch((error) => {
+                  });
+                  localStorage.clear('login');
+                  setTimeout(function(){
+                    window.location.href = 'index.html'
+                  },1000)
+            }
         },
         components: {
 
